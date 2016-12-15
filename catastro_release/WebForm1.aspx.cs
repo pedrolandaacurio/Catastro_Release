dc87288199952ecafa3a496dac4b090a1b74a85e
pedrolandaacurio <pedrolandaacurio@hotmail.com>
2016-12-14 19:05:18 -0500

    al 14

--- catastro_release/WebForm1.aspx.cs
+++ catastro_release/WebForm1.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace catastro_release
@@ -16,7 +17,14 @@ namespace catastro_release
 
         protected void Unnamed_ServerClick(object sender, EventArgs e)
         {
-
+            HtmlTable table = (HtmlTable)Page.FindControl("Table1");
+            foreach (HtmlTableRow row in table.Rows)
+            {
+                foreach (HtmlTableCell cell in row.Cells)
+                {
+                    
+                }
+            }
         }
     }
 }
